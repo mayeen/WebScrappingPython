24670ea316dff4a1c20cc0fd50dc291b2a10273c
Shahriar <km.shahriar28@gmail.com>
2022-06-19 23:41:51 +0200

    first commit: fetching data from the website

---
+++ scrape.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+from csv import writer 
+
+url = "https://www.pararius.com/apartments/amsterdam?ac=1"
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+sectionList = soup.findAll('section', class_= "listing-search-item")
+
+
+for section in sectionList:
+    title = section.find('a', class_="listing-search-item__link--title").text.replace('\n','')
+    location = section.find('div', class_="listing-search-item__location").text.replace('\n','')
+    price = section.find('span', class_="listing-search-item__price").text.replace('\n','')
+    surfaceArea = section.find('li', class_="illustrated-features__item--surface-area").text.replace('\n','')
+    info= [title, location, price, surfaceArea]
+    print(info)
+  
+    
